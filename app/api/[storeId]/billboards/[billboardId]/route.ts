91bbc3bad78b4b62d15eae055f598a57dc569435
Abir Santra <santraabir8@gmail.com>
2024-04-26 11:11:13 +0530

    create billboard endpoints

---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,128 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { billboardId: string } }
+) {
+  try {
+    if (!params.billboardId) {
+      return new NextResponse("Missing param: billboardId", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findUnique({
+      where: {
+        id: params.billboardId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_GET]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!label) {
+      return new NextResponse("Missing field: label", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Missing field: label", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Missing param: storeId", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Missing param: billboardId", { status: 400 });
+    }
+
+    const storeById = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId: userId,
+      },
+    });
+
+    if (!storeById) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.updateMany({
+      where: {
+        id: params.billboardId,
+      },
+      data: {
+        label: label,
+        imageUrl: imageUrl,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_PATCH]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Missing param: storeId", { status: 400 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse("Missing param: billboardId", { status: 400 });
+    }
+
+    const storeById = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId: userId,
+      },
+    });
+
+    if (!storeById) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.deleteMany({
+      where: {
+        id: params.billboardId,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_DELETE]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
---
+++ app/api/[storeId]/billboards/route.ts
@@ -0,0 +1,75 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { label, imageUrl } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 401 });
+    }
+
+    if (!label) {
+      return new NextResponse("Missing field: label", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Missing field: imageUrl", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Missing param: storeId", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId: userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label: label,
+        imageUrl: imageUrl,
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARDS-POST] ", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse("Missing param: storeId", { status: 400 });
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log("[BILLBOARDS-GET] ", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
